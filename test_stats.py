ab52d03abd701c7a93c84dbcca4b0d40039ebd54
Jeff Grass <jagrass@wisc.edu>
2015-08-27 13:52:38 -0500

    insert comment here

--- test_stats.py
+++ test_stats.py
@@ -1,7 +1,7 @@
 from stats import mean 
 # do not need to call stats.py
 
-from nose.tools import assert_equal
+from nose.tools import assert_equal, assert_almost_equal
 
 def test_mean():
         assert_equal(mean([2,4]), 3) 
@@ -20,6 +20,6 @@ def test_float_mean():
 ###
 
 def test_neg_mean():
-	assert_equal(mean([-2,2,4]), 1.333)
+	assert_almost_equal(mean([-2,2,4]), 1.333, places=3)
 # test_neg_mean()
 
