d78d0ab82664847cd96f95131ab6d5e493371ab4
Jeff Grass <jagrass@wisc.edu>
2015-08-27 14:12:51 -0500

    version N+1

--- test_stats.py
+++ test_stats.py
@@ -1,4 +1,4 @@
-from stats import mean 
+from stats import mean, std
 # do not need to call stats.py
 
 from nose.tools import assert_equal, assert_almost_equal
@@ -23,3 +23,9 @@ def test_neg_mean():
 	assert_almost_equal(mean([-2,2,4]), 1.333, places=3)
 # test_neg_mean()
 
+###
+
+def test_std1():
+	obs = std([0.0,0.2])
+	exp = 1.0
+	assert_egual(obs,std)
